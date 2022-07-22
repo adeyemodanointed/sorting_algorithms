f590ccca9146a2f3bd27afe953ddf7bc1d8a910f
Adeyemo <adeyemodanointed@yahoo.com>
2022-07-22 04:55:49 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - This program implements the bubble sort algorithm
+ *
+ * @array: the array of integers to be sorted
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+	int temp;
+	size_t i, j, k;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
